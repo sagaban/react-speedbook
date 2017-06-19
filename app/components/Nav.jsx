d3a2df5cd0cd4717532f1a3b9d747ec98ccd7fdb
Santiago Bandiera <santiagobandiera@yahoo.com.ar>
2017-06-19 16:03:08 -0300

    Add all page nav link. Add todo list

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -5,6 +5,13 @@ const Nav = () => (
   <nav className="nav">
     <NavLink to="/" brand>ReactSpeed</NavLink>
     <NavLink to="/book">Book</NavLink>
+    <NavLink to="/charts">Charts</NavLink>
+    <NavLink to="/embeds">Embeds</NavLink>
+    <NavLink to="/forms">Forms</NavLink>
+    <NavLink to="/custom">Custom</NavLink>
+    <NavLink to="/workflow">Workflow</NavLink>
+    <NavLink to="/buttons">Buttons</NavLink>
+    <NavLink to="/ajax">Ajax</NavLink>
     <NavLink
       href="https://github.com/manavsehgal/react-speed-book"
     >
